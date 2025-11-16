eb1c4763aab03656adf422a2baf4adeb2e7afcbd
Paiva <danpaiva@live.com>
2025-11-16 13:35:36 -0300

    feat: ajuste banco de dados campo nulo

---
+++ ArcusTel.PortalClientes/ArcusTel.PortalClientes.Api/Migrations/20251116163519_AjusteCampoNulo.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArcusTel.PortalClientes.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteCampoNulo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastPartnerRawStatus",
+                table: "TB_DidActivationRequest",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastPartnerRawStatus",
+                table: "TB_DidActivationRequest",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
